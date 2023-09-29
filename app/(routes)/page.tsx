be36125da70c347700a4891849488f0081412301
panduramadhan2 <panduramadhan2@gmail.com>
2023-09-29 14:29:00 +0700

    Ini Untuk Store Ecommerce NExt JS

---
+++ app/(routes)/page.tsx
@@ -0,0 +1,21 @@
+import getBillboard from "@/actions/get-billboard";
+import Billboard from "@/components/billboard";
+import Container from "@/components/ui/container";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const billboard = await getBillboard("a6bbcd9a-2f9e-4aab-a764-162d63afade0");
+  // console.log(billboard);
+
+  return (
+    <Container>
+      {/* <div className="space-y-10 pb-10"> */}
+      <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+        <Billboard data={billboard} />
+      </div>
+    </Container>
+  );
+};
+
+export default HomePage;
